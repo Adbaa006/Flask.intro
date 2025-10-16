4b07188e6db4e10486c3e1bca24d5351a35904ea
Adbaa006 <adbaa007@hotmail.com>
2025-10-16 09:45:19 +0200

    Laget liste

--- starter/app.py
+++ starter/app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+posts [
+   [1, "Første innlegg", "<p>Hei! 1</p>"]
+   [2, "Andre innlegg", "<p>Hei! 2</p>"]
+   [3, "Tredje innlegg", "<p>Hei! 3</p>"]
+]
+
 if __name__ == "__main__":
     app.run(debug=True)
-
