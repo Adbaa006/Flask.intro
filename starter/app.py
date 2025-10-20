742c51effff277e2cd34941b2404eb49719ddecc
Adbaa006 <adbaa007@hotmail.com>
2025-10-20 10:55:48 +0200

    Linker, men ingen innhold

--- starter/app.py
+++ starter/app.py
@@ -12,10 +12,24 @@ def hello():
     ]
     return render_template('index.html', posts=posts)
 
-@app.route('/post/<int:post_id>')
-def post1():
-      return render_template('post.html')
+@app.route("/post/<int:post_id>")
+def post_detail(post_id):
+    post = get_post_by_id(post_id)
+    if not post:
+        abort(404)
+    return render_template("post.html", post=post)
 
+from flask import abort
+
+def get_post_by_id(post_id: int):
+    for p in posts:
+        if p[0] == post_id:
+            return p
+    return None
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
